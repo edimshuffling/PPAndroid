65d2875b3c5cb17f5f651c85930a415ca2a299cd
edimshuffling <edimshuffling@gmail.com>
2017-01-04 18:44:56 +0100

    Large update

--- PPAndroid/PPAndroid.py
+++ PPAndroid/PPAndroid.py
@@ -1,29 +1,57 @@
-import seccure
 import kivy
 kivy.require('1.9.1') # replace with your current kivy version !
 
 from kivy.app import App
+from kivy.properties import StringProperty
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
+import os
 
+class Connected(Screen):
+    def disconnect(self):
+        self.manager.transition = SlideTransition(direction="right")
+        self.manager.current = 'login'
+        self.manager.get_screen('login').resetForm()
 
-class LoginScreen(GridLayout):
+class Login(Screen):
+    def do_login(self, loginText, passwordText):
+        app = App.get_running_app()
+        app.username = loginText
+        app.password = passwordText
+        self.manager.transition = SlideTransition(direction="left")
+        self.manager.current = 'connected'
+        app.config.read(app.get_application_config())
+        app.config.write()
 
-    def __init__(self, **kwargs):
-        super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 1
-        self.add_widget(Label(text='Username:', font_size=50))
-        self.username = TextInput(multiline=False, font_size=50, height=25)
-        self.add_widget(self.username)
-        self.add_widget(Label(text='Password:', font_size=50))
-        self.password = TextInput(password=True, multiline=False, font_size=50, height=25)
-        self.add_widget(self.password)
+    def resetForm(self):
+        self.ids['login'].text = ""
+        self.ids['password'].text = ""
 
+class LoginApp(App):
+    username = StringProperty(None)
+    password = StringProperty(None)
 
+    def build(self):
+        manager = ScreenManager()
+        manager.add_widget(Login(name='login'))
+        manager.add_widget(Connected(name='connected'))
+        return manager
+
+    def get_application_config(self):
+        if(not self.username):
+            return super(LoginApp, self).get_application_config()
+
+        conf_directory = self.user_data_dir + '/' + self.username
+
+        if(not os.path.exists(conf_directory)):
+            os.makedirs(conf_directory)
+
+        return super(LoginApp, self).get_application_config(
+            '%s/config.cfg' % (conf_directory))
 
 class MyApp(App):
-
     def build(self):
         return LoginScreen()
 
